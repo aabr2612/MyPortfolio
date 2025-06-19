d07036064f46398386df600d5bf5e834a545fb8f
aabr2612 <aabr2612@gmail.com>
2025-06-20 01:26:56 +0500

    Navbar completed

--- js/script.js
+++ js/script.js
@@ -1,5 +1,11 @@
 // toggle icon navbar
+let menuIcon = document.querySelector('#menu-icon');
+let navbar = document.querySelector('.navbar');
 
+menuIcon.onclick = () => {
+    menuIcon.classList.toggle('bx-x');
+    navbar.classList.toggle('active');
+}
 
 
 // scroll sections
@@ -23,6 +29,11 @@ window.onscroll = () => {
         }
     });
 
+    // sticky header
     let header = document.querySelector('header');
     header.classList.toggle('sticky', window.scrollY > 100);
+
+    // remove toggle icon and navbar when click navbar link (scroll)
+    menuIcon.classList.remove('bx-x');
+    navbar.classList.remove('active');
 }
